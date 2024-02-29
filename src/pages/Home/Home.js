c1d384f84cc6ed52134173ec245da768adbe61c1
shubham sahu <shubham.sahu@stem.com>
2024-03-01 01:01:35 +0530

    add react high chart

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,29 @@
-// Component for the Home page
-const Home = () => (
+import React from "react";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+
+const Home = () => {
+  // Define the chart options
+  const chartOptions = {
+    title: {
+      text: "Sample Chart Title",
+    },
+    series: [
+      {
+        data: [1, 2, 3, 4, 5],
+      },
+    ],
+  };
+
+  return (
     <div>
       <h2>Home</h2>
       <p>Welcome to the home page!</p>
+
+      {/* HighchartsReact component */}
+      <HighchartsReact highcharts={Highcharts} options={chartOptions} />
     </div>
   );
+};
 
 export default Home;
